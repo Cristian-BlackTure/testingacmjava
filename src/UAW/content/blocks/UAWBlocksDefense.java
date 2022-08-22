15abd84fb95b890f4319446b69064644d1a8adcc
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-22 17:07:22 +0300

    Update UAWBlocksDefense.java

--- src/UAW/content/blocks/UAWBlocksDefense.java
+++ src/UAW/content/blocks/UAWBlocksDefense.java
@@ -31,7 +31,7 @@ import static mindustry.type.ItemStack.with;
 public class UAWBlocksDefense {
 	public static Block placeholder,
 	// Gatling
-	matterfuser, spitfire, equalizer,
+        spitfire, equalizer,
 	// Sniper/Railgun
 	ashlock, longbow, deadeye,
 	// Shotcannon
@@ -47,99 +47,6 @@ public class UAWBlocksDefense {
 	public static void load() {
 
 		//region Serpulo
-		matterfuser = new ItemTurret("matterfuser") {{
-			requirements(Category.turret, with(
-				Items.copper, 115,
-				Items.lead, 120,
-				Items.graphite, 80
-			));
-			size = 3;
-			scaledHealth = 160;
-
-			reload = 6f;
-			recoil = 1f;
-			recoilTime = reload * 4;
-			maxAmmo = 30;
-
-			range = 20 * tilesize;
-			shootCone = 15f;
-			inaccuracy = 7.5f;
-			rotateSpeed = 10f;
-
-			ammoUseEffect = Fx.casing2Double;
-
-			shoot = new ShootAlternate() {{
-				barrels = 2;
-				shots = 2;
-				barrelOffset = 5;
-				spread = 4f;
-			}};
-
-			ammo(
-				UAWItems.matterialmatter, new BasicBulletType(5f, 9) {{
-					height = 9f;
-					width = 7f;
-					lifetime = 60f;
-					shootEffect = Fx.shootSmall;
-					smokeEffect = Fx.shootSmallSmoke;
-					ammoMultiplier = 2;
-				}}
-				Items.titanium, new TrailBulletType(10f, 10f) {{
-					height = 12f;
-					width = 5f;
-					frontColor = UAWPal.titaniumFront;
-					backColor = UAWPal.titaniumBack;
-					pierceArmor = true;
-					shootEffect = Fx.shootSmallColor;
-					smokeEffect = Fx.shootBigSmoke;
-					ammoMultiplier = 3;
-					trailLengthScale = 0.6f;
-				}},
-				Items.pyratite, new BasicBulletType(5f, 15) {{
-					height = 12f;
-					width = 8f;
-					frontColor = Pal.lightishOrange;
-					backColor = Pal.lightOrange;
-					status = StatusEffects.burning;
-					hitEffect = new MultiEffect(Fx.hitBulletSmall, Fx.fireHit);
-					ammoMultiplier = 5;
-					splashDamage = 10f;
-					splashDamageRadius = 22f;
-					makeFire = true;
-					lifetime = 60f;
-				}},
-				Items.lead, new BasicBulletType(5f, 15) {{
-					height = 12f;
-					width = 8f;
-					frontColor = UAWPal.cryoFront;
-					hitColor = backColor = UAWPal.cryoBack;
-					status = StatusEffects.freezing;
-					hitEffect = new MultiEffect(Fx.hitBulletColor, UAWFx.cryoHit);
-					ammoMultiplier = 5;
-					splashDamage = 10f;
-					splashDamageRadius = 22f;
-					lifetime = 60f;
-				}}
-			);
-			limitRange();
-
-			squareSprite = false;
-			drawer = new DrawTurret(modTurretBase) {{
-				parts.addAll(
-					new RegionPart("-barrel") {{
-						progress = PartProgress.recoil;
-						moveY = -5f * px;
-						heatProgress = PartProgress.reload;
-
-					}},
-					new RegionPart("-body"),
-					new RegionPart("-back") {{
-						progress = PartProgress.reload;
-						moveY = -6f * px;
-					}}
-				);
-			}};
-		}};
 		spitfire = new ItemTurret("spitfire") {{
 			requirements(Category.turret, with(
 				Items.lead, 350,
